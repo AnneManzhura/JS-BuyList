90cbda8ab8f9c5e29bfa9f3dcf143b2bee99e8dc
AnneManzhura <manzhurik@gmail.com>
2017-10-06 14:38:36 +0300

    Minor improvements.

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 $(function(){
 
     var $list= $(".items");
-    var $needed_items= $(".needed_purchases");
-    var $bought_items= $(".done_purchases");
+    var $needed_items_list= $(".needed_purchases");
+    var $bought_items_list= $(".done_purchases");
     var $new_input= $(".input");
 
     var ONE_ROW_HTML = $(".one_row_template").html();
@@ -11,13 +11,13 @@ $(function(){
 
     function addItem(title){
         var $node = $(ONE_ROW_HTML);
-        var needed_item=$(ONE_NEEDED_ITEM_HTML);
-        var bought_item=$(ONE_BOUGHT_ITEM_HTML);
+        var $needed_item=$(ONE_NEEDED_ITEM_HTML);
+        var $bought_item=$(ONE_BOUGHT_ITEM_HTML);
         var quantity=1;
-        var $quantity_label=$node.find(".label_count");
-
+        var new_name=$node.find(".title_edit").val();
+        var $quantity_label= $node.find(".label_count");
+        
         $quantity_label.text(quantity);
-
         $node.find(".title").text(title);
 
         $list.append($node);
@@ -25,35 +25,44 @@ $(function(){
         $node.slideDown(function () {
             $node.show();
         });
-        $bought_items.append(bought_item);
-        $needed_items.append(needed_item);
-        bought_item.hide();
+        $bought_items_list.append($bought_item);
+        $needed_items_list.append($needed_item);
+        $bought_item.hide();
 
-        needed_item.find(".name").text(title);
-        needed_item.find(".circles").text(quantity);
-        bought_item.find(".name").text(title);
-        bought_item.find(".circles").text(quantity);
+        $needed_item.find(".name").text(title);
+        $needed_item.find(".circles").text(quantity);
+        $bought_item.find(".name").text(title);
+        $bought_item.find(".circles").text(quantity);
 
 
         $node.find(".but_plus").click(function () {
             quantity+=1;
-            needed_item.find(".circles").text(quantity);
-            bought_item.find(".circles").text(quantity);
+            $needed_item.find(".circles").text(quantity);
+            $bought_item.find(".circles").text(quantity);
             $node.find(".label_count").fadeOut(100, function () {
-            $quantity_label.text(quantity);
-            $node.find(".label_count").fadeIn(100);
+                if(quantity>9 && quantity<99){
+                    //$node.find(".label_count").width(20);
+                    $node.find(".label_count").addClass("center");
+                }
+                $quantity_label.text(quantity);
+                $node.find(".label_count").fadeIn(100);
             });
-
         });
 
         $node.find(".but_minus").click(function () {
             if (quantity > 1) {
                 quantity -= 1;
-                needed_item.find(".circles").text(quantity);
-                bought_item.find(".circles").text(quantity);
+                $needed_item.find(".circles").text(quantity);
+                $bought_item.find(".circles").text(quantity);
                 $node.find(".label_count").fadeOut(100, function () {
-                $quantity_label.text(quantity);
-                $node.find(".label_count").fadeIn(100);
+                    if(quantity<10){
+                        $node.find(".label_count").width(14);
+                    }
+                    if(quantity>9 && quantity<99){
+                        $node.find(".label_count").width(20);
+                    }
+                    $quantity_label.text(quantity);
+                    $node.find(".label_count").fadeIn(100);
                 });
             }
         });
@@ -62,61 +71,65 @@ $(function(){
             $node.slideUp(function () {
                 $node.remove();
             });
-            needed_item.remove();
-            bought_item.remove();
+            $needed_item.remove();
+            $bought_item.remove();
         });
 
         $node.find(".bl_toBuy").click(function () {
 
-            needed_item.hide();
-            bought_item.show();
+            $needed_item.hide();
+            $bought_item.show();
 
             $node.find(".inner_item").fadeOut(200, function () {
-                $node.find(".bl_toBuy").addClass("none");
+                $node.find(".bl_toBuy").hide();
                 $node.find(".but_minus").addClass("hidden");
                 $node.find(".but_plus").addClass("hidden");
                 $node.find(".title").addClass("crossed");
-                $node.find(".bl_delete").addClass("none");
-                $node.find(".bl_toUnBuy").addClass("visible");
+                $node.find(".bl_delete").hide();
+                $node.find(".bl_toUnBuy").show();
                 $node.find(".inner_item").fadeIn(200);
             });
         });
 
         $node.find(".bl_toUnBuy").click(function(){
-            needed_item.show();
-            bought_item.hide();
+            $needed_item.show();
+            $bought_item.hide();
             $node.find(".inner_item").fadeOut(200, function () {
-                $node.find(".bl_toBuy").removeClass("none");
-                $node.find(".bl_toUnBuy").removeClass("visible");
+                $node.find(".bl_toBuy").show();
+                $node.find(".bl_toUnBuy").hide();
                 $node.find(".but_minus").removeClass("hidden");
                 $node.find(".but_plus").removeClass("hidden");
                 $node.find(".title").removeClass("crossed");
-                $node.find(".bl_delete").removeClass("none");
+                $node.find(".bl_delete").show();
                 $node.find(".inner_item").fadeIn(200);
             });
-
-
         });
 
- /*       $node.find(".title").click(function(){
-            $node.find(".title").addClass("none");
-            $node.find(".title_edit").attr("value", title);
-
+        $node.find(".title").click(function(){
+            $node.find(".title").hide();
             $node.find(".title_edit").addClass("visible");
-            $node.find(".title_edit").focus();
+            $node.find(".title_edit").val(title);
 
+
+            /*node.find(".title_edit").attr("value", title);
+            node.find(".title_edit").addClass("visible");
+            node.find(".title_edit").focus();*/
         });
         $node.find(".title_edit").focusout(function(){
-            var new_name= $(this).val();
-            alert(new_name);
+
             $node.find(".title_edit").removeClass("visible");
-            $node.find(".title").text($(this).val());
-            $node.find(".title").removeClass("none");
+            //$node.find(".title").text(new_name);
+            $node.find(".title").show();
+        });
 
-        });*/
+/*
+            node.find(".title_edit").change(function () {
+                var new_name= $(this).val();
+                alert(new_name);
+            });*/
 
-
-    /*    $node.find(".title_edit").change(function () {
+/*
+        node.find(".title_edit").change(function () {
             var new_name= $(this).val();
             alert(new_name);
 
